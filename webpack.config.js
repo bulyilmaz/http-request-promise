21622991c3d952d4fb867e2dd88663043933deed
Bülent Yılmaz <bulyilmaz@gmail.com>
2019-06-02 12:41:05 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require("path");
 
 module.exports = {
+    mode: "production",
     entry: ["@babel/polyfill", "./src/js/index.es6"],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "js/bundle.js"
     },
     devServer: {
-        contentBase: "./dist"
+        contentBase: "./dist",
+        /*port: 8080,*/
+        /*index: "index.html"*/
     },
     plugins: [],
     module: {
